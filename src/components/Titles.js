dfd7870c35e2c6f396df58ca68e340435d973d5d
tybeewx <tybeewx@users.noreply.github.com>
2020-05-04 18:52:17 -0400

    Styling

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,8 +3,13 @@ import React from "react";
 const Titles = () => (
     <div>
         <h1 className="title-container__title"> Weather Finder</h1>
-        <h3 className="title-container__subtitle">
-        <marquee behavior="scroll" direction="left" scrollamount="10">Current temperature, weather conditions and more...</marquee></h3>
+        <h2 className="title-container__subtitle"></h2>
+        <marquee behavior="scroll" direction="left" scrollamount="5">
+            <h3> Current temperature</h3> </marquee>
+            <marquee behavior="scroll" direction="left" scrollamount="4">
+                <h4> Weather conditions</h4></marquee>
+                <marquee behavior="scroll" direction="left" scrollamount="3">
+                    <h5> Wind Speed </h5></marquee>
     </div>
 );
 export default Titles;
